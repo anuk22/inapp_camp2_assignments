393899b0080ff5c05715885a499fe2fc363cbf59
anuk22 <109217414+anuk22@users.noreply.github.com>
2022-07-19 15:00:35 +0530

    exercise_looping

--- exercise_loopassignment.py
+++ exercise_loopassignment.py
@@ -1,7 +1,4 @@
 #print the lst of nums which are divisible by 5 and multiple of 8 btw 2000 and 2500
-from termios import NL1
-
-
 print("Nums which are divisible by 5 and multiple of 8 btw 2000 and 2500 are:")
 for i in range(2000,2501):
     if(i%5==0 and i%8==0):
