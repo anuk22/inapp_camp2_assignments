e3347c9e8b13eaf407cebc472ff0f518f4d8ae2a
anuk22 <109217414+anuk22@users.noreply.github.com>
2022-07-27 00:01:29 +0530

    phonebookdatabase

--- filedb.py
+++ filedb.py
@@ -1,7 +1,6 @@
-from tkinter import EXCEPTION
 import pyodbc
 import sys
-conString='Driver={SQL Server};Server=DESKTOP-MIIL0EB\SQLEXPRESS;Database=employee_db;Trusted_Connection=yes;'
+conString='Driver={SQL Server};Server=DESKTOP-R1N5RGM;Database=Employee;Trusted_Connection=yes;'
 myconn=pyodbc.connect(conString)
 mycursor=myconn.cursor()
 try:
@@ -16,7 +15,7 @@ try:
     mycursor.execute("INSERT INTO PhoneBook VALUES(?,?)",("Manu",7896541236))
     mycursor.execute("INSERT INTO PhoneBook VALUES(?,?)",("Devin",9632587410))
     mycursor.execute("INSERT INTO PhoneBook VALUES(?,?)",("Manya",9510263487))
-    '''
+  '''
     mycursor.execute("SELECT * FROM PhoneBook")
     myconn.commit()
     
@@ -43,6 +42,7 @@ def add():
         return
     try:
         mycursor.execute("INSERT INTO PhoneBook VALUES(?,?)",(name,number))
+        myconn.commit()
     except Exception as e:
         print(f"{type(e).__name__}")
 def delete():
@@ -62,15 +62,48 @@ def SearchByName():
     nm=input("Enter the name you want to get details :")
     try:
         mycursor.execute("SELECT name,contact FROM PhoneBook WHERE name=(?)",(nm))
-        
     except Exception as e:
         print(f"{type(e).__name__}")
+    else:
+        contacts=mycursor.fetchall()
+        if(len(contacts)>0):
+            for i,j in contacts:
+                print(i,"  ",j)
+            
+        else:
+            print(f"{nm} is not found")
+myconn.commit()
 
-
-list()
-#add()
-#delete()
-SearchByName()
-list()
+def SearchByNumber():
+    nm=int(input("Enter the number you want to get details :"))
+    try:
+        mycursor.execute("SELECT name,contact FROM PhoneBook WHERE contact=(?)",(nm))
+    except Exception as e:
+        print(f"{type(e).__name__}")
+    else:
+        contacts=mycursor.fetchall()
+        if(len(contacts)>0):
+            for i,j in contacts:
+                print(i,"  ",j)
+            
+        else:
+            print(f"{nm} doesnt exists in the phonebook")
+myconn.commit()
 myconn.close()
 
+while True:
+    ch=int(input("""Enter Your Choice
+                     1.List All Contacts
+                     2.Add A contact
+                     3.Delete A contact
+                     4.Search a contact by its name
+                     5.Search a contact by its number
+                     6.Exit"""))
+    match ch:
+        case 1:list()
+        case 2:add()
+        case 3:delete()
+        case 4:SearchByName()
+        case 5:SearchByContact()
+        case 6:Exit()
+        case _:print("Invalid Input")
